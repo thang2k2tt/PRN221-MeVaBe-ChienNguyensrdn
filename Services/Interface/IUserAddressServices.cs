9e7c062a7f2ebecfd18f6168abd7eb5d5aed20b1
Net05_PhanHieuNghia <nghiaphce171837@fpt.edu.vn>
2024-07-14 16:25:52 +0700

    add user address page

---
+++ Services/Interface/IUserAddressServices.cs
@@ -0,0 +1,18 @@
+﻿using PRN221_MeVaBe_Repo.DTOs;
+using PRN221_MeVaBe_Repo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface IUserAddressServices
+    {
+        Task<List<UserAddress>> GetAllUserAddres();
+        Task AddAddress(CreateAddressDTO addressDTO);
+        Task UpdateAddress(UpdateAddressDTO addressDTO);
+        Task DeleteAddress(int id);
+    }
+}
