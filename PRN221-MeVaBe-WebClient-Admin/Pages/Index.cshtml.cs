f5f66382a05118762686af59a11e166f8c4da1dd
Thang2k2la <thang2k2la@gmail.com>
2024-07-20 20:13:12 +0700

    change db

--- PRN221-MeVaBe-WebClient-Admin/Pages/Index.cshtml.cs
+++ PRN221-MeVaBe-WebClient-Admin/Pages/Index.cshtml.cs
@@ -1,19 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN221_MeVaBe_Repo.Interfaces;
 
 namespace PRN221_MeVaBe_WebClient_Adminn.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private IUnitOfWork unitOfWork;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            this.unitOfWork = unitOfWork;
         }
-
+        public int ProductCount { get; set; }
+        public int UserActive { get; set; }
+        public double? Rate { get; set; }
         public void OnGet()
         {
+            var product = unitOfWork.ProductRepository.Get();
+            ProductCount = product.Count();
+            var user = unitOfWork.UserRepository.Get(filter: u => u.Status == true);
+            UserActive = user.Count();
+            var feedback = unitOfWork.FeedbackRepository.Get();
+            double? sum = 0;
+            if (feedback.Count() > 0)
+            {
+                foreach (var items in feedback)
+                {
+                    sum += items.Rate;
+                }
+                Rate = (sum / feedback.Count())*10;
+            }
 
         }
     }
