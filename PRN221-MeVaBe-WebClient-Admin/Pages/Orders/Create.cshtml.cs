b07ee4ec0e5013a5d76f7d093abcece2ed12c481
Thang2k2la <thang2k2la@gmail.com>
2024-07-20 08:49:49 +0700

    layout admin

---
+++ PRN221-MeVaBe-WebClient-Admin/Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PRN221_MeVaBe_Repo.Models;
+
+namespace PRN221_MeVaBe_WebClient_Adminn.Pages.Orders
+{
+    public class CreateModel : PageModel
+    {
+        private readonly PRN221_MeVaBe_Repo.Models.DBContext _context;
+
+        public CreateModel(PRN221_MeVaBe_Repo.Models.DBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["UserId"] = new SelectList(_context.TblUsers, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public OrderDetail OrderDetail { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.TblOrderDetails.Add(OrderDetail);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ PRN221-MeVaBe-WebClient-Admin/Pages/Orders/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PRN221_MeVaBe_Repo.Interfaces;
+using PRN221_MeVaBe_Repo.Models;
+
+namespace PRN221_MeVaBe_WebClient_Adminn.Pages.Orders
+{
+    public class IndexModel : PageModel
+    {
+        private IUnitOfWork unitOfWork;
+        public IndexModel(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IList<OrderDetail> OrderDetail { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            OrderDetail = (IList<OrderDetail>)unitOfWork.OrderDetailRepository.Get(includeProperties:"User");
+        }
+    }
+}
